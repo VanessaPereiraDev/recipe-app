74915ac7b705fe4051330319bf877ccdc0bd9f79
Vanessa Pereira <pereira.van2001@gmail.com>
2022-08-28 11:27:57 +0100

    Link all cards to the respective detail page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,18 @@
 import Popular from "../components/Popular";
 import Vegan from "../components/Vegan";
-import React from 'react'
+import { motion } from 'framer-motion';
 
 function Home() {
   return (
-    <div>
+    <motion.div
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+    >
         <Vegan />
         <Popular />
-    </div>
+    </motion.div>
   )
 }
 
